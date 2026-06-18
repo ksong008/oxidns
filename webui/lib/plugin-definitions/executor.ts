1e5bf12b16a551b1b3696a0fffac93067475b15b
ksong008 <ksong008@gmail.com>
2026-06-18 13:39:11 +0800

    update reject soa webui help

--- webui/lib/plugin-definitions/executor.ts
+++ webui/lib/plugin-definitions/executor.ts
@@ -57,7 +57,8 @@ export const executorPluginDefinitions: PluginKindDefinition[] = [
                 "定义规则命中后要执行的动作，可引用执行器或使用 accept、return、reject、jump、goto、mark 等内置动作。",
               label: "执行动作",
               type: "text",
-              placeholder: "$forward_main / accept / reject 3 / reject 0 soa / jump seq_tag",
+              placeholder:
+                "$forward_main / accept / reject 0 / reject 0 soa / reject 3 / jump seq_tag",
             },
           ],
         },
